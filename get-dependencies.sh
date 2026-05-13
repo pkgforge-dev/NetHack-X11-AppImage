34f3936e27c5ee9fedc7633225fa663d10f48095
Link4Electronics <link4electronics@gmail.com>
2026-05-13 16:41:05 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -16,6 +16,7 @@ get-debloated-pkgs --add-common --prefer-nano
 # make-aur-package
 
 # If the application needs to be manually built that has to be done down here
+mkdir -p ./AppDir/bin
 VERSION=5.0.0
 wget https://github.com/NetHack/NetHack/archive/refs/tags/NetHack-${VERSION}_Released.tar.gz
 tar -xvf ./*.tar.gz
@@ -27,4 +28,5 @@ cd sys/unix
 cd ../..
 make fetch-lua
 path -NP1 -i ../2ndpatch.patch
-make -j
+make -j$(nproc)
+mv -v src/nethack dat/* ../AppDir/bin
